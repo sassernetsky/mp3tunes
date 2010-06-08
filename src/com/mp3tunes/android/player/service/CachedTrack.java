0cf3b84c2bc774a22a8318e7f30dc059d9cd53c3
sonec <sonec@localhost>
2010-06-08 14:44:19 -0700

    downloads cached track to temp file now

--- src/com/mp3tunes/android/player/service/CachedTrack.java
+++ src/com/mp3tunes/android/player/service/CachedTrack.java
@@ -1,34 +1,46 @@
 package com.mp3tunes.android.player.service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import com.binaryelysium.mp3tunes.api.HttpClientCaller;
 import com.binaryelysium.mp3tunes.api.Id;
 import com.binaryelysium.mp3tunes.api.InvalidSessionException;
 import com.binaryelysium.mp3tunes.api.LockerId;
 import com.binaryelysium.mp3tunes.api.RemoteMethod;
 import com.binaryelysium.mp3tunes.api.Track;
+import com.binaryelysium.util.FileUtils;
 import com.mp3tunes.android.player.LocalId;
+import com.mp3tunes.android.player.Music;
 
 public class CachedTrack implements Track
 {
     Track    mTrack;
     String   mCachedUrl;
     String   mCachedPath;
+    File     mCachedFile;
     String   mFormat;
     int      mBitrate;
     int      mStatus;
     Progress mProgress;
     String   mError;
     
-    public CachedTrack(Track t, String path, String url, String format, int bitrate)
+    public CachedTrack(Track t, String format, int bitrate) throws AlreadyDownloadedException
     {
         mTrack      = t;
-        mCachedPath = path;
-        mCachedUrl  = url;
         mFormat     = format;
         mBitrate    = bitrate;
-        mStatus     = Status.created;
         mProgress   = new Progress();
         mError      = "no error";
+        try {
+            setCachePath();
+            setUrlFromCachePath();
+        } catch (IOException e) {
+            mError      = "Failed to create cached track";
+            mStatus     = Status.failed;
+        }
     }
     
     public CachedTrack(LocalId id, Track t)
@@ -41,6 +53,50 @@ public class CachedTrack implements Track
         mStatus     = Status.finished;
         mProgress   = new Progress();
         mProgress.mProgress = 100;
+        mError      = "no error";
+    }
+    
+    private void setUrlFromCachePath()
+    {
+        mCachedUrl = HttpServer.pathToUrl(mCachedPath);
+    }
+    
+    private String encode(String text)
+    {
+        text = text.replace("/", "_slash_");
+        text = text.replace(".", "_dot_");
+        text = text.replace("%", "_percent_");
+        return text;
+    }
+    
+    private File createFile(String dir, boolean tmp)
+    {
+        String name = encode(getFileKey() + "_" + Integer.toString(mBitrate));
+        if (tmp) name += "_tmp";
+        name += "." + mFormat;
+        File file = new File(dir, name);
+        return file;
+    }
+    
+    private void setCachePath() throws IOException, AlreadyDownloadedException
+    {
+        String dir  = Music.getMP3tunesCacheDir();
+        File   file = createFile(dir, false);
+        if (file.exists()) {
+            mCachedPath = file.getAbsolutePath();
+            setStatus(Status.finished);
+            mProgress.mProgress = 100;
+            return;
+        }
+        
+        file = createFile(dir, true);
+        if (file.createNewFile()) {
+            mCachedPath = file.getAbsolutePath();
+            setStatus(Status.created);
+            return; 
+        }
+        
+        throw new AlreadyDownloadedException(file.getAbsolutePath());
     }
     
     public int getAlbumId()
@@ -135,6 +191,37 @@ public class CachedTrack implements Track
         return mStatus;
     }
     
+    synchronized public void cacheTrack()
+    {
+        if (mStatus == CachedTrack.Status.finished ) {
+            if (mCachedPath.endsWith("_tmp." + mFormat)) {
+                String newFile = mCachedPath.replaceAll("_tmp." + mFormat, "." + mFormat);
+                try {
+                    FileUtils.copyFile(mCachedPath, newFile);
+                    mCachedPath = newFile;
+                    setUrlFromCachePath();
+                } catch (IOException e) {
+                    Logger.log(e);
+                }
+            }
+        }
+    }
+    
+    //For now the caller is responsible for making sure that nothing else is reading the file
+    synchronized public boolean deleteTmpCopy()
+    {
+        String fileName = mCachedPath;
+        if (!mCachedPath.endsWith("_tmp." + mFormat))
+            fileName = mCachedPath.replace("." + mFormat, "_tmp." + mFormat);
+        else
+            mStatus = Status.failed;
+        File file = new File(fileName);
+        if (file.exists()) {
+            return file.delete();
+        }
+        return true;
+    }
+    
     public static final class Status
     {
         public static final int created     = 0;
@@ -179,5 +266,19 @@ public class CachedTrack implements Track
     public String getErrorMessage()
     {
         return mError;
+    }
+    
+    public class AlreadyDownloadedException extends Exception
+    {
+        private String mPath;
+        private static final long serialVersionUID = -7663492585836784742L;
+        public AlreadyDownloadedException(String path)
+        {
+            mPath = path;
+        }
+        public String getPath()
+        {
+            return mPath;
+        }
     }
 }
