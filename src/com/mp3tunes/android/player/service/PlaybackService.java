62f457adc9409de93682a88e002d66e2bd202a7f
sonec <sonec@localhost>
2010-09-09 11:09:57 -0700

    fixes buffering at end of playlist once and for all--- once and for all

--- src/com/mp3tunes/android/player/service/PlaybackService.java
+++ src/com/mp3tunes/android/player/service/PlaybackService.java
@@ -437,13 +437,29 @@ public class PlaybackService extends Service
     
     int getState(long pos, long duration)
     {
+        try {
+            if (!mIsStarting) {
+                Log.w("Mp3Tunes", "!mIsStarting");
+                if (mChangingTrackAction == null) {
+                    Log.w("Mp3Tunes", "mChangingTrackAction == null");
+                    if (mPlaybackQueue.getPlaybackTrack() == null) {
+                        Log.w("Mp3Tunes", "mPlaybackQueue.getPlaybackTrack() == null");
+                        throw new Exception();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            return PlaybackState.State.DONE;
+        }
+        
         if (mIsStarting) {
             return PlaybackState.State.STARTING;
         } else if (mChangingTrackAction != null) {
             return PlaybackState.State.CHANGING_TRACK;
         } else if (!(pos > 0 && duration > 0 && pos <= duration)) {
             return PlaybackState.State.BUFFERING;
-        }
+        } 
+            
         return PlaybackState.State.PLAYING;
     }
     
