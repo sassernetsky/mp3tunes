ea1de33bead667b5c1f53d997159ce91b0d08d00
Ron Welte <Ron Welte@localhost>
2010-10-14 10:19:50 -0700

    Added the push.sh script to the repository.

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#hg push https://mp3tunesadam%40gmail.com:vQ6ku6CD5Ej8@mp3tunes.googlecode.com/hg/mp3tunes
+
+hg push https://mp3tunesron%40gmail.com:wE6UR3FT7em4@mp3tunes.googlecode.com/hg/mp3tunes
+
